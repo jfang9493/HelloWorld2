3684e598180b3b8706566f242d709d8ee9470a35
BT_1N3_28 <@>
2019-01-03 10:14:35 -0500

    sjtfj

---
+++ src/sample/GameCode.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+
+public class GameCode {
+    int step = 0;
+    public void startGame(ActionEvent actionEvent)
+    {
+
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 public class Main extends Application {
 
@@ -12,12 +13,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 200, 250));
         primaryStage.show();
-    }
 
+
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
